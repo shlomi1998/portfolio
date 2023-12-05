be143d896c9d4f8112797575329ff23b5b254967
shlomi azenkot <80357892+shlomi1998@users.noreply.github.com>
2023-12-05 14:34:12 +0200

    Update data.js

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,91 +1,88 @@
-import Image1 from '../../assets/project1.jpg'
-import Image2 from '../../assets/project2.jpg'
-import Image3 from '../../assets/project3.jpg'
-import Image4 from '../../assets/project4.jpg'
-import Image5 from '../../assets/project5.jpg'
-import Image6 from '../../assets/project6.jpg'
-import Image7 from '../../assets/project7.jpg'
-import Image8 from '../../assets/project8.jpg'
-
+import Image1 from "../../assets/project1.jpg";
+import Image2 from "../../assets/project2.jpg";
+import Image3 from "../../assets/project3.jpg";
+import Image4 from "../../assets/project4.jpg";
+import Image5 from "../../assets/project5.jpg";
+import Image6 from "../../assets/project6.jpg";
+import Image7 from "../../assets/project7.jpg";
+import Image8 from "../../assets/project8.jpg";
 
 const data = [
-    {
-        id: 1,
-        category: 'uiux',
-        image: Image1,
-        title: "Workie (UIUX)",
-        desc: "הביקוש הגובר לעבודה מיידית וקצרת מועד בישראל יצר חוסר התאמה לפלטפורמות קיימות לחיפוש עבודה המותאמות למשרות קבועות. כדי להתמודד עם זה,פיתחתי את Workie - פלטפורמת אינטרנט ומובייל המתמקדת אך ורק בהזדמנויות עבודה מיידיות",
-        demo: 'https://www.figma.com/proto/UASZqvozPrPSpf6IRRAv3u/Workie---Quick-Jobs?node-id=186-1059&starting-point-node-id=186%3A1059',
-        github: 'https://github.com/egattor'
-    },
-    {
-        id: 2,
-        category: 'frontend',
-        image: Image2,
-        title: "Nike (Frontend)",
-        desc: "אתר nike רספונסיבי שילוב של React+TypeScript+Tailwind+Vite",
-        demo: 'https://github.com/shlomi1998/Nike-Website-React.-TypeScript-Tailwind',
-        github: 'https://github.com/shlomi1998/Nike-Website-React.-TypeScript-Tailwind'
-    },
-    {
-        id: 3,
-        category: 'full stack',
-        image: Image1,
-        title: "Workie(Full Stack)",
-        desc: `workie זה פלטפורמת אינטרנט ומובייל המתמקדת בהזדמנויות עבודה מיידיות הפלטפורמה  כתובה בשלל שפות וספריות  Scss(Bem)+Mui+Tailwind+React(Redux.js+Context)+TypeScript+Socket.io+Node.js(express)+MongoDB`,
-        demo: 'https://www.youtube.com/watch?v=_7FP9JmaKmE&t=21s',
-        github: 'https://github.com/shlomi1998/workie-fe-be'
-    },
-    {
-        id: 4,
-        category: 'full stack',
-        image: Image3,
-        title: "Blog App (Full Stack)",
-        desc: "אתר blog נחמד שבנוי ב React+Nod.js+MySql (יש כניסה למשתמש ורישום בנוסף ניתן להעלות תמונה וכן יש שימוש ב-Rich Text Editor)",
-        demo: 'https://www.youtube.com/watch?v=5lTh9tgRrO8',
-        github: 'https://github.com/shlomi1998/Blog-App-React-Node.js-MySQL'
-    },
-    
-    
-   
-    // {
-    //     id: 5,
-    //     category: 'uiux',
-    //     image: Image5,
-    //     title: "Project Title Five (UIUX)",
-    //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
-    //     demo: 'http://egatortutorials.com',
-    //     github: 'https://github.com/egattor'
-    // },
-    // {
-    //     id: 6,
-    //     category: 'frontend',
-    //     image: Image6,
-    //     title: "Project Title Six (Frontend)",
-    //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-    //     demo: 'http://egatortutorials.com',
-    //     github: 'https://github.com/egattor'
-    // },
-    // {
-    //     id: 7,
-    //     category: 'frontend',
-    //     image: Image7,
-    //     title: "Project Title Seven (Frontend)",
-    //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-    //     demo: 'http://egatortutorials.com',
-    //     github: 'https://github.com/egattor'
-    // },
-    
-    // {
-    //     id: 9,
-    //     category: 'frontend',
-    //     image: Image9,
-    //     title: "Project Title Nine (Frontend)",
-    //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-    //     demo: 'http://egatortutorials.com',
-    //     github: 'https://github.com/shlomi1998/workie-fe-be/tree/main/workie'
-    // }
-]
+  {
+    id: 1,
+    category: "uiux",
+    image: Image1,
+    title: "Workie (UIUX)",
+    desc: "הביקוש הגובר לעבודה מיידית וקצרת מועד בישראל יצר חוסר התאמה לפלטפורמות קיימות לחיפוש עבודה המותאמות למשרות קבועות. כדי להתמודד עם זה,פיתחתי את Workie - פלטפורמת אינטרנט ומובייל המתמקדת אך ורק בהזדמנויות עבודה מיידיות",
+    demo: "https://www.figma.com/proto/UASZqvozPrPSpf6IRRAv3u/Workie---Quick-Jobs?node-id=186-1059&starting-point-node-id=186%3A1059",
+    github: "https://github.com/egattor",
+  },
+  {
+    id: 2,
+    category: "frontend",
+    image: Image2,
+    title: "Nike (Frontend)",
+    desc: "אתר nike רספונסיבי שילוב של React+TypeScript+Tailwind+Vite",
+    demo: "https://github.com/shlomi1998/Nike-Website-React.-TypeScript-Tailwind",
+    github:
+      "https://github.com/shlomi1998/Nike-Website-React.-TypeScript-Tailwind",
+  },
+  {
+    id: 3,
+    category: "backend",
+    image: Image4,
+    title: "Workie(Full Stack)",
+    desc: "workie זה פלטפורמת אינטרנט ומובייל המתמקדת בהזדמנויות עבודה מיידיות הפלטפורמה  כתובה בשלל שפות וספריות  Scss(Bem)+Mui+Tailwind+React(Redux.js+Context)+TypeScript+Socket.io+Node.js(express)+MongoDB",
+    demo: "https://www.youtube.com/watch?v=_7FP9JmaKmE&t=21s",
+    github: "https://github.com/shlomi1998/workie-fe-be",
+  },
+  {
+    id: 4,
+    category: "backend",
+    image: Image3,
+    title: "Blog App (Full Stack)",
+    desc: "אתר blog נחמד שבנוי ב React+Nod.js+MySql (יש כניסה למשתמש ורישום בנוסף ניתן להעלות תמונה וכן יש שימוש ב-Rich Text Editor)",
+    demo: "https://www.youtube.com/watch?v=5lTh9tgRrO8",
+    github: "https://github.com/shlomi1998/Blog-App-React-Node.js-MySQL",
+  },
 
+  // {
+  //     id: 5,
+  //     category: 'uiux',
+  //     image: Image5,
+  //     title: "Project Title Five (UIUX)",
+  //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
+  //     demo: 'http://egatortutorials.com',
+  //     github: 'https://github.com/egattor'
+  // },
+  // {
+  //     id: 6,
+  //     category: 'frontend',
+  //     image: Image6,
+  //     title: "Project Title Six (Frontend)",
+  //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+  //     demo: 'http://egatortutorials.com',
+  //     github: 'https://github.com/egattor'
+  // },
+  // {
+  //     id: 7,
+  //     category: 'frontend',
+  //     image: Image7,
+  //     title: "Project Title Seven (Frontend)",
+  //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+  //     demo: 'http://egatortutorials.com',
+  //     github: 'https://github.com/egattor'
+  // },
 
-export default data
+  // {
+  //     id: 9,
+  //     category: 'frontend',
+  //     image: Image9,
+  //     title: "Project Title Nine (Frontend)",
+  //     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+  //     demo: 'http://egatortutorials.com',
+  //     github: 'https://github.com/shlomi1998/workie-fe-be/tree/main/workie'
+  // }
+];
+
+export default data;
